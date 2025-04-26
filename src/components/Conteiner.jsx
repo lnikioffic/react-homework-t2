6c3b74510ff58d6f8ac523653d0b9cc5d44b7603
lniki <nik_ivanov_33@mail.ru>
2025-04-26 16:09:06 +0500

    add component Conteiner

---
+++ src/components/Conteiner.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import Greeting from "./Greeting";
+import Header from "./Header";
+import Clock from "./Clock";
+
+// Массив случайных имен
+const randomNames = [
+  "Алексей",
+  "Мария",
+  "Иван",
+  "Елена",
+  "Дмитрий",
+  "Анна",
+  "Сергей",
+  "Ольга",
+];
+
+const getRandomName = () => {
+  const randomIndex = Math.floor(Math.random() * randomNames.length);
+  return randomNames[randomIndex];
+};
+
+const Conteiner = () => {
+  const [name, setName] = useState(getRandomName());
+  const [prevName, setPrevName] = useState(name);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setPrevName(name);
+      setName(getRandomName());
+    }, 10000);
+
+    return () => clearInterval(intervalId);
+  }, [name]);
+
+  return (
+    <>
+      <Header />
+      <Greeting name={name} prevName={prevName} />
+      <Clock />
+    </>
+  );
+};
+
+export default Conteiner;
