597da5aabcc4729f948357eaadec0ea2add213b3
lniki <nik_ivanov_33@mail.ru>
2025-05-08 14:48:52 +0500

    fix use useRef

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,14 +1,22 @@
+import { useRef, useEffect } from "react";
 import "../App.css";
 
-function Greeting(props) {
-  const isNewName = props.prevName === props.name;
+function Greeting({ name }) {
+  const prevName = useRef(name);
+
+  // обновление
+  useEffect(() => {
+    prevName.current = name;
+  }, [name]);
+
+  const isNewName = prevName.current === name;
 
   return (
     <div className="read-the-docs">
       {isNewName ? (
-        <h3>Привет, {props.name}!</h3>
+        <h3>Привет, {name}!</h3>
       ) : (
-        <h3>Привет, у тебя поменялось имя, теперь ты {props.name}!</h3>
+        <h3>Привет, у тебя поменялось имя, теперь ты {name}!</h3>
       )}
     </div>
   );
