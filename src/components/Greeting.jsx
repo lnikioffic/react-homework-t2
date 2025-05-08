bea4f220d0bdc29b105e7061d57e5c6c520def17
lniki <nik_ivanov_33@mail.ru>
2025-05-08 14:36:37 +0500

    fix name varibles

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,12 +1,11 @@
 import "../App.css";
 
 function Greeting(props) {
-
-  const hasNameChanged = props.prevName === props.name;
+  const isNewName = props.prevName === props.name;
 
   return (
     <div className="read-the-docs">
-      {hasNameChanged ? (
+      {isNewName ? (
         <h3>Привет, {props.name}!</h3>
       ) : (
         <h3>Привет, у тебя поменялось имя, теперь ты {props.name}!</h3>
